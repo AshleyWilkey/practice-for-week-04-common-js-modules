5868ea38d7e52549fbf0a59f5b4bc19fce4d53c9
Ashley Wilkey <contact@ashleywilkey.com>
2024-11-12 11:42:49 -0800

    finish import problems

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-
 // Your code here
+
+const { msg1, msg2, msg3 } = require("./messages");
+const { sayHelloTo, giveMessageToMrsPotato } = require("./send-messages");
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
---
+++ send-messages/index.js
@@ -0,0 +1,7 @@
+const sayHelloTo = require("./say-hello-to");
+const giveMessageToMrsPotato = require("./give-message-to-mrs-potato");
+
+module.exports = {
+  sayHelloTo,
+  giveMessageToMrsPotato,
+};
